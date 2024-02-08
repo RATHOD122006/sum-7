5776e4179aeefc2dfeb46947115c6e300c571a92
RATHOD122006 <sumitrathod8225@gmail.com>
2024-02-08 17:28:29 +0530

    sum

---
+++ sum1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int n;
+	scanf("%d",&n);
+	int sum;
+	for(int a=1;a<=n;a++){
+		sum+=a;
+		
+	}
+	printf("%d",sum);
+}
---
+++ sum2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int n;
+	scanf("%d", &n);
+	int sum=1;
+	for(int a=1;a<=n;a++){
+		sum=sum*a;
+		
+	}
+	printf("%d",sum);
+}
